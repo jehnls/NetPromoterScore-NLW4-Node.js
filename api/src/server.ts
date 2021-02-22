718ee63a343f5ee081ca6705811447516aa0e19e
jehnls <jeferson_nls@hotmail.com>
2021-02-22 20:39:21 -0300

    Primeira aula, instalado as  dependencia (express, type-express, typescript,), config packjson e criando rotas

---
+++ api/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/",(request, response) =>{
+
+    return response.json({ message: "Hello World"})
+})
+
+
+app.post("/",(request, response) =>{
+
+    return response.json({ message: "Hello World"})
+})
+
+app.listen(3333)
